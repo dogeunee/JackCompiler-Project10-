ca7e9355d460b082fe425b0a4cac1e850fe62f9c
dogeun <mckin711@snu.ac.kr>
2024-10-18 23:47:11 +0900

    add directory recursion

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include "include/CompilationEngine.h"
 #include <filesystem>
 #include <iostream>
+#include <vector>
+
+namespace fs = std::filesystem;
+
 string changeToXML(string str1)
 {
     std::filesystem::path filePath(str1);
@@ -12,8 +16,33 @@ string changeToXML(string str1)
 int main(int argc, char *argv[])
 {
     string str1 = argv[1];
-    CompilationEngine compEngine(str1, changeToXML(str1));
-    compEngine.compileClass();
+    fs::path p(str1);
+    vector<fs::path> FileList;
+    if (fs::is_regular_file(p))
+    {
+        CompilationEngine compEngine(str1, changeToXML(str1));
+        compEngine.compileClass();
+    }
+    else if (fs::is_directory(p))
+    {
+        for (const auto &entry : fs::directory_iterator(p))
+        {
+            if (fs::is_regular_file(entry))
+            {
+                fs::path filePath = entry.path();
+                if (filePath.extension() == ".jack")
+                {
+                    FileList.push_back(filePath);
+                }
+            }
+        }
+        for (fs::path file : FileList)
+        {
+            CompilationEngine compEngine(file.string(), changeToXML(file.string()));
+            compEngine.compileClass();
+        }
+    }
+
     return 0;
 }
 /*{
