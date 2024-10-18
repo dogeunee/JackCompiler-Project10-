fdb6f7cb54ef1035a7e6f53e8217d96035afc835
dogeunee <mckin711@snu.ac.kr>
2024-10-18 23:51:34 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,15 +12,32 @@ string changeToXML(string str1)
 int main(int argc, char *argv[])
 {
     string str1 = argv[1];
-    CompilationEngine compEngine(str1, changeToXML(str1));
-    compEngine.compileClass();
+    fs::path p(str1);
+    vector<fs::path> FileList;
+    if (fs::is_regular_file(p))
+    {
+        CompilationEngine compEngine(str1, changeToXML(str1));
+        compEngine.compileClass();
+    }
+    else if (fs::is_directory(p))
+    {
+        for (const auto &entry : fs::directory_iterator(p))
+        {
+            if (fs::is_regular_file(entry))
+            {
+                fs::path filePath = entry.path();
+                if (filePath.extension() == ".jack")
+                {
+                    FileList.push_back(filePath);
+                }
+            }
+        }
+        for (fs::path file : FileList)
+        {
+            CompilationEngine compEngine(file.string(), changeToXML(file.string()));
+            compEngine.compileClass();
+        }
+    }
+
     return 0;
 }
-/*{
-    JackTokenizer jack;
-    jack.initFilename("test/Main.jack");
-    while (jack.hasMoreTokens())
-    {
-        jack.advance();
-    }
-}*/
